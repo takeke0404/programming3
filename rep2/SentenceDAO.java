e48344db4e0a3f10461791e9557e940110bad428
Rintaro-Yamada <49467661+Rintaro-Yamada@users.noreply.github.com>
2019-10-23 20:11:32 +0900

    データ重複を回避できるようにしました

--- rep2/SentenceDAO.java
+++ rep2/SentenceDAO.java
@@ -3,7 +3,7 @@ import java.sql.*;
 import java.io.*;
 
 public class SentenceDAO{
-    public List<Sentence>findAll(){  //レコードの取得
+    public List<Sentence> findAll(){  //レコードの取得
 	Connection conn=null;
 	List<Sentence>senList=new ArrayList<Sentence>();
 	try{
@@ -20,20 +20,23 @@ public class SentenceDAO{
 		Sentence sentence=new Sentence(name);
 		senList.add(sentence);
 	    }
-	}catch(SQLException e){
-	    e.printStackTrace();
-	    return null;
-	}catch(ClassNotFoundException e){
-	    e.printStackTrace();
-	    return null;
-	}finally{
-
-	    return senList;
-	}
+	    }catch(SQLException e){
+	        e.printStackTrace();
+	    }catch(ClassNotFoundException e){
+	        e.printStackTrace();
+        }finally{
+            return senList;
+	    }
     }
 
     
-    public List<Sentence> insert(Sentence x) {  //レコードの挿入
+    public List<Sentence> insert(Sentence x) { //レコードの挿入
+        if (hantei(x)) {
+            System.out.println(x.getName() + "はすでに存在しています。");
+            List<Sentence>senList=new ArrayList<Sentence>();
+	        senList = findAll();
+	        return senList;
+        }
         Connection conn = null;
         try {
             Class.forName("org.h2.Driver");
@@ -48,7 +51,7 @@ public class SentenceDAO{
 	   
         } catch (SQLException e) {
             e.printStackTrace();
-            System.out.println("name:" + x.getName()+ "はすでに存在しています。");
+            
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -57,7 +60,13 @@ public class SentenceDAO{
 	return senList;
     }
 
-    public List<Sentence> delete(Sentence x) {  //レコードの削除
+    public List<Sentence> delete(Sentence x) { //レコードの削除
+        if (!hantei(x)) {
+            System.out.println(x.getName()+ "は存在していません。");
+            List<Sentence>senList=new ArrayList<Sentence>();
+	        senList = findAll();
+	        return senList;
+        }
         Connection conn = null;
         try {
             Class.forName("org.h2.Driver");
@@ -72,7 +81,7 @@ public class SentenceDAO{
 	   
         } catch (SQLException e) {
             e.printStackTrace();
-            System.out.println("name:" + x.getName()+ "は存在していません。");
+            
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -103,39 +112,48 @@ public class SentenceDAO{
 	return senList;
     }
 
-    public List<Sentence> reset() {   //レコードの初期化
-	deleteALL();
+    public List<Sentence> reset() { //レコードの初期化
+        deleteALL();
         Connection conn = null;
         try {
             Class.forName("org.h2.Driver");
 
             conn = DriverManager.getConnection("jdbc:h2:~/rep2/database", "sa", "");
 
-	    try {    
-		String fileName = "dataset_example.txt"; // ファイル名指定
+            try {
+                String fileName = "dataset_example.txt"; // ファイル名指定
 
-		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF-8"));
+                BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF-8"));
 
-		// 変数lineに1行ずつ読み込むfor文
-		for (String line = in.readLine(); line != null; line = in.readLine()) {
-		    String sql = "INSERT INTO SENTENCE VALUES(?)";
-		    PreparedStatement pStmt = conn.prepareStatement(sql);
-		    pStmt.setString(1, line);
-		    pStmt.executeUpdate();
-		}
-		in.close();  // ファイルを閉じる
-	    }catch (IOException e) {
-		e.printStackTrace(); // 例外が発生した所までのスタックトレースを表示
-	    }
-	    conn.close();	   
-	}catch (SQLException e) {
+                // 変数lineに1行ずつ読み込むfor文
+                for (String line = in.readLine(); line != null; line = in.readLine()) {
+                    String sql = "INSERT INTO SENTENCE VALUES(?)";
+                    PreparedStatement pStmt = conn.prepareStatement(sql);
+                    pStmt.setString(1, line);
+                    pStmt.executeUpdate();
+                }
+                in.close(); // ファイルを閉じる
+            } catch (IOException e) {
+                e.printStackTrace(); // 例外が発生した所までのスタックトレースを表示
+            }
+            conn.close();
+        } catch (SQLException e) {
             e.printStackTrace();
-	} catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
-	}
-    List<Sentence>senList=new ArrayList<Sentence>();
-    senList = findAll();
-    return senList;
+        }
+        List<Sentence> senList = new ArrayList<Sentence>();
+        senList = findAll();
+        return senList;
+    }
+
+    boolean hantei(Sentence x) {
+        List<Sentence> senList = findAll();
+        for (Sentence sen : senList) {
+            if (x.getName().equals(sen.getName()))
+                return true;
+        }
+        return false;
     }
 }
 
