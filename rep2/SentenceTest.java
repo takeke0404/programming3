ff3a47d4b0cc99a1efccfaad696ab6cd3cd00626
Rintaro YAMADA <clf14134@cse.cs.nitech.ac.jp>
2019-10-23 13:59:25 +0900

    細かい修正を加えました

--- rep2/SentenceTest.java
+++ rep2/SentenceTest.java
@@ -2,13 +2,13 @@ import java.util.*;
 import java.sql.*;
 public class SentenceTest {
     public static void main(String args[]) {
-        SentenceDAO senDAO= new SentenceDAO();
+    SentenceDAO senDAO= new SentenceDAO();
 	Sentence x=new Sentence("student is a kind of human");	
-	List<Sentence> senList= senDAO.findAll();
-	senList=senDAO.delete(x);    //レコードの削除(複数ある場合1つ)
-	senList=senDAO.insert(x);      //レコードの追加
-	//senList=senDAO.deleteALL();  //レコード全削除
-	//senList=senDAO.reset();    //レコードの初期化(dataset_example.txt)
+	List<Sentence> senList= senDAO.findAll(); //レコードの取得
+	//senList=senDAO.delete(x);	//レコードの削除(複数ある場合1つ)
+	//senList=senDAO.insert(x);	//レコードの追加
+	//senList=senDAO.deleteALL();	//レコード全削除
+	//senList=senDAO.reset();	//レコードの初期化(dataset_example.txt)
 	for (Sentence sen: senList) {
 	    System.out.println(sen.getName());
 	}
