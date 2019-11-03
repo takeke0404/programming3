311b71bce22ceeaa6d81f9fe838ae5668b7392f3
takeke <takeke0404@yahoo.co.jp>
2019-11-03 19:08:30 +0900

    セマンティックのインスタンスを追加

--- rep3/SemanticNet/Example.java
+++ rep3/SemanticNet/Example.java
@@ -7,26 +7,26 @@ public class Example {
     public static void main(String args[]){
 	SemanticNet sn = new SemanticNet();
 
-	// 野球はスポーツである．
-	sn.addLink(new Link("is-a","baseball","sports",sn));
+	// Macbookはlaptopである．
+	sn.addLink(new Link("is-a","Macbook","laptop",sn));
 
-	// 太郎は名古屋工業大学の学生である．
-	sn.addLink(new Link("is-a","Taro","NIT-student",sn));
+	// 弘太は名古屋工業大学の学生である．
+	sn.addLink(new Link("is-a","Kota","NIT-student",sn));
 
-	// 太郎の専門は人工知能である．
-	sn.addLink(new Link("speciality","Taro","AI",sn));
-	
-	// フェラーリは車である．
-	sn.addLink(new Link("is-a","Ferrari","car",sn));
+	// 弘太の専門は人工知能である．
+	sn.addLink(new Link("speciality","Kota","AI",sn));
 
-	// 車はエンジンを持つ．
-	sn.addLink(new Link("has-a","car","engine",sn));
-	
-	// 太郎の趣味は野球である．
-	sn.addLink(new Link("hobby","Taro","baseball",sn));
-	
-	// 太郎はフェラーリを所有する．
-	sn.addLink(new Link("own","Taro","Ferrari",sn));
+	// Macbookはcomputerである．
+	sn.addLink(new Link("is-a","Macbook","computer",sn));
+
+	// computerはcpuを持つ．
+	sn.addLink(new Link("has-a","computer","cpu",sn));
+
+	// 弘太の趣味はアクアリウムである．
+	sn.addLink(new Link("hobby","Kota","Aquarium",sn));
+
+	// kotaはMacbookを所有する．
+	sn.addLink(new Link("own","Kota","Macbook",sn));
 
 	// 名古屋工業大学の学生は，学生である．
 	sn.addLink(new Link("is-a","NIT-student","student",sn));
@@ -38,10 +38,9 @@ public class Example {
 	sn.printNodes();
 
 	ArrayList<Link> query = new ArrayList<Link>();
-	query.add(new Link("own","?y","Ferrari"));
+	query.add(new Link("own","?y","Macbook"));
 	query.add(new Link("is-a","?y","student"));
-	query.add(new Link("hobby","?y","baseball"));
+	query.add(new Link("hobby","?y","Aquarium"));
 	sn.query(query);
-    }    
+    }
 }
-
