80ffdf6e4e7ceccee1dff8c67d0550f75ed16fc4
Rintaro-Yamada <49467661+Rintaro-Yamada@users.noreply.github.com>
2019-12-02 11:30:01 +0900

    ブロックのコンストラクタクラス

---
+++ rep5/Block.java
@@ -0,0 +1,12 @@
+class Block {    
+    private String name;
+    private String color;
+    private String shape;
+
+    //　コンストラクタ
+    public Block(String name,String color,String shape) {        
+        this.name=name;
+        this.color=color;
+        this.shape=shape;
+    }
+}
