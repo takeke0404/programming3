3f39825a6f9f6a19a4c4ba90eab2f9941ec5e307
Rintaro-Yamada <49467661+Rintaro-Yamada@users.noreply.github.com>
2019-12-02 21:16:23 +0900

    GUIに渡すリストの完成

--- rep5/GuiTest.java
+++ rep5/GuiTest.java
@@ -9,26 +9,25 @@ import java.awt.event.*;
 
 public class GuiTest extends JFrame {
 
-	private JPanel contentPane;
+    private JPanel contentPane;
 
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    GuiTest frame = new GuiTest();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GuiTest frame = new GuiTest();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
+    /**
 	 * Create the frame.
 	 */
 	public GuiTest() {
@@ -51,19 +50,52 @@ public class GuiTest extends JFrame {
 
 		//planの解析
 		String[] lines = output.toString().split("\n");
-		boolean flag = false;
+        boolean flag = false;
+        String ini="";
 		ArrayList<ArrayList<String[]>> st = new ArrayList<>();
 		for (int i = 0; i < lines.length; i++) {
-			if (flag&&lines[i].contains("initialState")) {
-                st.add(getArrays(lines[i].replace("initialState:","")));
-			}
+            if (flag && lines[i].contains("initialState")) {
+                ini = lines[i].replace("initialState:", "").replace("[", "").replace("]", "");
+                st.add(getArrays(ini));
+            }
+            
+            else if (flag && lines[i].contains("pick up")) {    //pick up はGUIで表示するか未定(今は前の状態と同じにしている)
+                st.add(st.get(st.size() - 1));
+                
+            }
+            
+            else if (flag && lines[i].contains("Place")) {
+                String[] s = lines[i].replace("Place ", "").split(" on ");
+                if (s[0].equals(s[1])) {    //Place A on A　などのときはとりあえず前の状態と同じにしている。このエラーが解消できたらこのif文は消して良い。
+                    st.add(st.get(st.size() - 1));  //この１行コメントアウトすることで、GUI上ではこの問題を解消できるけどね・・・
+                    continue;
+                }
+                ini=ini.replace(", ontable "+s[0], "");
+                ini = ini.replace(", clear " + s[1], "");
+                ini += ", " + s[0] + " on " + s[1];
+                st.add(getArrays(ini));
+            }
+            
+            else if (flag && lines[i].contains("remove")) {
+                String[] s = lines[i].replace("remove ", "").split(" from on top ");
+                if (s[0].equals(s[1])) {    //remove A from on top A　などのときはとりあえず前の状態と同じにしている。このエラーが解消できたらこのif文は消して良い。
+                    st.add(st.get(st.size() - 1)); //この１行コメントアウトすることで、GUI上ではこの問題を解消できるけどね・・・
+                    continue;
+                }
+                ini +=", ontable "+s[0];
+                ini += ", clear " + s[1];
+                ini = ini.replace(", " + s[0] + " on " + s[1], "");
+                st.add(getArrays(ini));
+            }
 			if(lines[i].equals("***** This is a plan! *****")){
 				flag = true;
             }
-		}
+        }
+
 
         //コンソールに表示
-        for(int i=0;i<st.size();i++){
+        for (int i = 0; i < st.size(); i++) {
+            System.out.println("fase" + i);
             for(int j=0;j<st.get(i).size();j++){
                 for(int k=0;k<st.get(i).get(j).length;k++){
                     System.out.print(st.get(i).get(j)[k]);
@@ -81,28 +113,29 @@ public class GuiTest extends JFrame {
     状態 [clear A, clear C, A on B, ontable B, ontable C, handEmpty]から
     配列のリスト[BA][C]を作成
     */
-    private ArrayList<String[]> getArrays(String line){
-        String[] state=line.replace("[","").replace("]","").split(",");
+    private ArrayList<String[]> getArrays(String line) {
+        String[] state = line.split(",");
         ArrayList<ArrayList<String>> list = new ArrayList<>();
         ArrayList<String> tmp = new ArrayList<>();
-        while (tmp.size() < state.length-1) {
+        while (tmp.size() < state.length - 1) {
             for (int j = 0; j < state.length; j++) {
-                if(!tmp.contains(state[j])&&state[j].contains("ontable")){
+                if (!tmp.contains(state[j]) && state[j].contains("ontable")) {
                     tmp.add(state[j]);
                     ArrayList<String> a = new ArrayList<>();
-                    a.add(state[j].replace(" ontable ",""));
+                    a.add(state[j].replace(" ontable ", ""));
                     list.add(a);
                     continue;
                 }
-                if(!tmp.contains(state[j])&&state[j].contains("clear")){
+                if (!tmp.contains(state[j]) && state[j].contains("clear")) {
                     tmp.add(state[j]);
                     continue;
                 }
-                if(!tmp.contains(state[j])&&state[j].contains("on")){
-                    for(int k=0;k<list.size();k++){
-                        if(state[j].contains(list.get(k).get(list.get(k).size()-1))){
+                if (!tmp.contains(state[j]) && state[j].contains("on")) {
+                    for (int k = 0; k < list.size(); k++) {
+                        if (state[j].contains(list.get(k).get(list.get(k).size() - 1))) {
                             tmp.add(state[j]);
-                            list.get(k).add(state[j].replace(" on "+list.get(k).get(list.get(k).size()-1),"").replace(" ",""));
+                            list.get(k).add(state[j].replace(" on " + list.get(k).get(list.get(k).size() - 1), "")
+                                    .replace(" ", ""));
                         }
                     }
                 }
@@ -110,7 +143,7 @@ public class GuiTest extends JFrame {
         }
 
         ArrayList<String[]> a = new ArrayList<>();
-        for(int j=0;j<list.size();j++){
+        for (int j = 0; j < list.size(); j++) {
             a.add(list.get(j).toArray(new String[list.get(j).size()]));
         }
 
